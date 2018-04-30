97a80f1d955550c9251074e733e1b4fd32e58e31
User <User@User-PC>
2018-04-30 09:59:19 +0900

    2번째 변경됨

--- src/com/example/GitTest.java
+++ src/com/example/GitTest.java
@@ -3,6 +3,6 @@ package com.example;
 public class GitTest {
 	
 	public static void main(String [] args) {
-		System.out.println("도망간 안재원 구합니다.");
+		System.out.println("도망간 안재원 구합니다. 안재원 관리 담당 구합니다.");
 	}
 }

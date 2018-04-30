795fe113a964b6e20cf8add24db4ba1779f7e8be
User <User@User-PC>
2018-04-30 10:19:39 +0900

    바꿈

--- src/com/example/GitTest.java
+++ src/com/example/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
 	public static void main(String [] args) {
 		System.out.println("도망간 안재원 구합니다. 안재원 관리 담당 구합니다.");
 		System.out.println("팀장");
-		System.out.println("팀원");
+		System.out.println("팀원-충돌 처리 완료");
 	}
 }

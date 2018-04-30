72385750131b63a4c188c49b4461a9c64d95ff89
User <User@User-PC>
2018-04-30 10:23:34 +0900

    kkk

--- src/com/example/GitTest.java
+++ src/com/example/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
 		System.out.println("도망간 안재원 구합니다. 안재원 관리 담당 구합니다.");
 		System.out.println("팀장");
 		System.out.println("팀원-충돌 처리 완료");
-		System.out.println("문정호-충돌 처리 완료");
+		System.out.println("문정호-충돌 처리 완료jlkjlj");
 	}
 }
